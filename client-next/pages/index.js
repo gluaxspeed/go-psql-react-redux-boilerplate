c33f2ea33e5eeebf3b4952063044fc96c8631aea
Jonathan Pavlik <jonathanpavlik@host-vlep1-26.dhcp.stevens-tech.edu>
2018-03-27 14:42:08 -0400

    have to rework attempt to add private routes due to way nextjs works

--- client-next/pages/index.js
+++ client-next/pages/index.js
@@ -1,14 +1,16 @@
 import Head from 'next/head';
 import React from 'react';
 
-import { Link } from '../utils/routes';
-
+import { Link } from '../utils';
 class Index extends React.Component {
 
   render() {
     return (
       <div>
         <p>Hello World!</p>
+        <Link href="/test">
+          <a>test</a>
+        </Link>
       </div>
     );
   }
